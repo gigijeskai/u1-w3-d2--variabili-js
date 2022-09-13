d373947fbc10f745a1a7569d40b659ffc4e54ce5
gigijeskai <luigindaco@gmail.com>
2022-09-13 15:32:38 +0200

    esercizio variabili

---
+++ js/file.js/esercizi.js
@@ -0,0 +1,48 @@
+let nome = "Mirtilla";
+console.log(nome);
+var utente = "cliente";
+console.log(utente);
+const PI = 3.14;
+console.log(PI);
+
+//nome = "Lilla";
+nome = "Lilla"; //cambiato il valore della variabile "nome"
+console.log(nome);
+utente = "amministratore";
+console.log(utente);
+//PI = 4;
+//console.log(PI);
+const PiSbagliato = 4;
+console.log(PiSbagliato);
+document.write(`${'nome'} ${'utente'} e il PI Sbagliato ${PiSbagliato}`);
+//^^concatenazione con dati tipo stringa e numerici con backtick
+
+(PI == PiSbagliato);
+console.log((PI == PiSbagliato)); //booleano falso
+(PI < PiSbagliato);
+console.log((PI < PiSbagliato)); //booleano vero
+
+// const giorniAnno = 365;
+// giorniAnno = 364;
+// console.log(giorniAnno); \\commentata per non avere l'errore
+
+let saluto;
+saluto = ' \n\ ciao \n\ ';
+saluto = 123 ;
+document.write(saluto);  
+//^^ variabile dichiarata , assegnata e cambiata di valore e tipo di dato  
+
+let parola1 = "buon";
+let parola2 = "giorno";
+console.log(parola1);
+console.log(parola2); //addizione stringhe
+
+let salutoDefinitivo = parola1 + parola2;
+console.log(salutoDefinitivo);
+
+let primoNumero = 20;
+let secondoNumero = 5;
+let stagioni = primoNumero / secondoNumero;
+console.log(stagioni); //addizione e divisione
+
+
